9c0fead8e86e9706e2f6b481acbef41bdbb43959
Fflam <Fflameq@gmail.com>
2024-09-21 05:37:07 -0700

    Update Service.php

--- Service.php
+++ Service.php
@@ -213,6 +213,15 @@ class Service implements ServiceInterface
             $username = $externalUser->username;
         }
 
+        $isampuser = Service::api('/Core/GetUserInfo', [
+            'UID' => $user->username;
+        ])
+
+        if($isampuser->failed())
+        {
+            throw new \Exception("[ServiceAMP] Failed to find user in isampuser function $isampuser->failed()");
+        }
+		
         $server = Service::api('/ADSModule/DeployTemplate', [
             'TemplateID' => $package->data('template'),
             'NewUsername' => $username, 
@@ -243,7 +252,7 @@ class Service implements ServiceInterface
             // finally, lets email the user their login details
             $user->email([
                 'subject' => 'Game Panel Account',
-                'content' => "Your account has been created on the game panel. You can login using the following details: <br><br> Username: {$username} <br> Password: {$password}",
+                'content' => "Your account has been created on the game panel. You can login using the following details: <br><br> Username: {$username} <br> Password: {$password} <br><br><br> test output {$isampuser}",
                 'button' => [
                     'name' => 'Game Panel',
                     'url' => settings('ServiceAMP::hostname'),
