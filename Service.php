898f6c3bd1aa76ddaf204184ca0aa302ea4bde23
Fflam <Fflameq@gmail.com>
2024-09-24 00:50:19 -0700

    Update Service.php

--- Service.php
+++ Service.php
@@ -184,7 +184,7 @@ class Service implements ServiceInterface
                 'NewPassword' => $newPassword,
             ]);
             $var = "this is test data to print from change password AMP";
-            Log::debug($var, $test);
+            Log::emergency($var);
             //hrow new Exception("[ServiceAMP] test exception output!");
             
             if($response->failed())
